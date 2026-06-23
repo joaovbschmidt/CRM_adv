4122deff4d718dcdbd7f11bf281c05c6cb669e11
joaovbschmidt <joaovitorsschmidt@gmail.com>
2026-06-23 15:01:31 -0300

    configura integracao supabase

--- src/database/supabase.js
+++ src/database/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://cqgcgzhtfqlyifvhpprt.supabase.co';
-const supabaseKey = 'sb_publishable_qHMbAetEAsAeJVfGFH31Yg_HQOVJulb';
+const supabaseUrl = import.meta.env.SUPABASE_URL;
+const supabaseKey = import.meta.env.SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
